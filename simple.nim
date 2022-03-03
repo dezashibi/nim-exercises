fee3c2a2b831f7110223ffbe6b49111c64560131
Navid Dezashibi <dezashibi@gmail.com>
2022-03-03 22:25:23 +0400

    vars

---
+++ simple.nim
@@ -0,0 +1,24 @@
+echo "Hello world"
+echo 10/2
+echo "strings ", 10, 5+5, 6*2
+
+
+# This is a comment
+
+# variable
+var variableName = "Value"
+echo variableName
+
+var i = 12
+echo i + 10
+
+var j = 10.2
+echo j + 0.5
+
+# unchangable
+let version = 1.1
+
+# will make error
+# version = 2
+
+
