a2d7b7778241746a69ec132bcab7e4e334162c2f
Navid Dezashibi <dezashibi@gmail.com>
2022-03-04 11:05:36 +0400

    slices

---
+++ slice.nim
@@ -0,0 +1,10 @@
+var num1 = 10
+var someString = $num1 # string version of the num1
+
+echo "the number is " & someString
+
+var myName = "Navid"
+
+echo myName[3]
+echo myName[2..4]
+echo myName[2..<4]
