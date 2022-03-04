5829c63b69e9b55726cf0239828da1a8da1f194a
Navid Dezashibi <dezashibi@gmail.com>
2022-03-04 15:38:39 +0400

    more proc example

--- proc.nim
+++ proc.nim
@@ -17,3 +17,21 @@ proc multisum(numbers: varargs[int]): int =
 
 echo multisum(12, 13, 1, 3, 5)
 
+
+proc yes(question: string): bool =
+  echo question, " (y/n)"
+
+  while true:
+    case stdin.readLine:
+      of "y", "Y", "yes", "Yes":
+        return true
+      of "n", "N", "no", "No":
+        return false
+      else:
+        echo "Please be clear: yes or no"
+
+if yes("Should I delete all your important files?"):
+  echo "I'm sorry man, I'm afraid I can't do that!"
+else:
+  echo "I think you know what the problem is just as well as I do."
+
