ece662bf12e367d76b3158b5494fb0cd0ea673e1
Navid Dezashibi <dezashibi@gmail.com>
2022-03-04 15:14:21 +0400

    block and break example

---
+++ block.nim
@@ -0,0 +1,16 @@
+block myblock:
+  echo "entering block"
+  while true:
+    echo "looping"
+    break # leaves the loop, but not the block
+  echo "still in block"
+echo "outside the block"
+
+block myblock2:
+  echo "entering block"
+  while true:
+    echo "looping"
+    break myblock2 # leaves the block (and the loop)
+  echo "still in block" # it won't be printed
+echo "outside the block"
+
