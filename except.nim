30d68d461b6286a43ba37a7d6028a51960f63fa0
Navid Dezashibi <dezashibi@gmail.com>
2022-03-04 12:21:35 +0400

    except module

---
+++ except.nim
@@ -0,0 +1,10 @@
+import strutils except split
+import unicode 
+
+echo "Hello Woبrld!".split("ب".toRunes)
+
+#[
+#in here we chose that a proc with same name not to cause conflicts
+#but in case we need both we could do this
+#echo unicode.split(.....)
+#]#
