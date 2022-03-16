b91ae0cc0e70fccfb9f6825fc83b98e0150cfece
Navid Dezashibi <knavels.official@gmail.com>
2022-03-16 13:04:20 +0400

    c wrapper and sdl example

---
+++ c_wrapper.nim
@@ -0,0 +1,21 @@
+proc printf(format: cstring): cint {.importc, varargs, header: "stdio.h", discardable.}
+printf("My name is %s and I am %d years old!\n", "Navid", 35)
+
+proc displayFormatted(format: cstring): cint {.importc: "printf", varargs, header: "stdio.h", discardable .}
+displayFormatted("My name is %s and I am %d years old!\n", "Navid", 35)
+
+type
+    CTime = int64
+
+proc time(arg: ptr CTime): CTime {.importc, header: "<time.h>" .}
+
+type
+    TM {.importc: "struct tm", header: "<time.h>".} = object
+        tm_min: cint
+        tm_hour: cint
+
+proc localtime(time: ptr CTime): ptr TM {.importc, header: "<time.h>".}
+
+var seconds = time(nil)
+let tm = localtime(addr seconds)
+echo(tm.tm_hour, ":", tm.tm_min)
---
+++ sdllib.nim
@@ -0,0 +1,30 @@
+#[
+    install mingw msys64
+    download sdl from here: https://www.libsdl.org/download-2.0.php
+    extract and copy contents of x86_64-w64-mingw32 to the installation folder like C:\msys64\mingw64
+]#
+
+when defined(Windows):
+    const libName* = "SDL2.dll"
+elif defined(Linux):
+    const libName* = "libSDL2.so"
+elif defined(MacOsX):
+    const libName* = "libSDL2.dylib"
+
+type
+    SdlWindow = object
+    SdlWindowPtr* = ptr SdlWindow
+    SdlRenderer = object
+    SdlRendererPtr* = ptr SdlRenderer
+
+{.push dynlib: libName .}
+proc init*(flags: uint32): cint {.importc: "SDL_Init".}
+proc createWindowAndRenderer*(width, height: cint, window_flags: cuint, window: var SdlWindowPtr, renderer: var SdlRendererPtr): cint {.importc: "SDL_CreateWindowAndRenderer".}
+proc pollEvent*(event: pointer): cint {.importc: "SDL_PollEvent".}
+proc setDrawColor*(renderer: SdlRendererPtr, r, g, b, a: uint8): cint {.importc: "SDL_SetRenderDrawColor", discardable.}
+proc present*(renderer: SdlRendererPtr) {.importc: "SDL_RenderPresent".}
+proc clear*(renderer: SdlRendererPtr) {.importc: "SDL_RenderClear".}
+proc drawLines*(renderer: SdlRendererPtr, points: ptr tuple[x, y: cint], count: cint): cint {.importc: "SDL_RenderDrawLines", discardable.}
+{.pop.}
+
+const INIT_VIDEO* = 0x00000020
