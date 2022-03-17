801007e1cb56cbc8bee9b70f73614c1b78152132
Navid Dezashibi <knavels.official@gmail.com>
2022-03-17 16:09:09 +0400

    object property example, [] operator

--- obj_property.nim
+++ obj_property.nim
@@ -17,3 +17,32 @@ new s
 s.host = 34 # same as 'host='(s, 34)
 
 echo s.host
+
+
+type
+    Vector* = object
+        x, y, z: float
+
+proc `[]=`*(v: var Vector, i: int, value: float) =
+    # setter
+    case i
+    of 0: v.x = value
+    of 1: v.y = value
+    of 2: v.z = value
+    else: assert(false)
+
+proc `[]`*(v: Vector, i: int): float =
+    # getter
+    case i
+    of 0: result = v.x
+    of 1: result = v.y
+    of 2: result = v.z
+    else: assert(false)
+
+var myVector: Vector
+myVector[0] = 12
+myVector[1] = 10
+myVector[2] = 0
+
+echo myVector[0]
+echo myVector
