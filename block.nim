4ceaa872ab1a09d4bfb4752e4a2bbcfb8e98d3f9
Navid Dezashibi <knavels.official@gmail.com>
2022-03-18 09:59:29 +0400

    prettify using vscode plugin
    Nim Alt checks indentations in the beginning and make the rest of indentations match them

--- block.nim
+++ block.nim
@@ -1,20 +1,20 @@
 block myblock:
-  echo "entering block"
-  while true:
-    echo "looping"
-    break # leaves the loop, but not the block
-  echo "still in block"
+    echo "entering block"
+    while true:
+        echo "looping"
+        break # leaves the loop, but not the block
+    echo "still in block"
 echo "outside the block"
 
 block myblock2:
-  echo "entering block"
-  while true:
-    echo "looping"
-    break myblock2 # leaves the block (and the loop)
-  echo "still in block" # it won't be printed
+    echo "entering block"
+    while true:
+        echo "looping"
+        break myblock2 # leaves the block (and the loop)
+    echo "still in block" # it won't be printed
 echo "outside the block"
 
 echo "Hello block arg ":
-  let a = "Wor"
-  let b = "ld!"
-  a & b
+    let a = "Wor"
+    let b = "ld!"
+    a & b
--- calc.nim
+++ calc.nim
@@ -6,45 +6,45 @@ var num2: float
 var result: float
 
 while true:
-  echo "Choose your action: Addition, Subtraction, Multiplication, Division, Exit"
-  input = stdin.readLine
-  case input:
-    of "Exit":
-      break
+    echo "Choose your action: Addition, Subtraction, Multiplication, Division, Exit"
+    input = stdin.readLine
+    case input:
+        of "Exit":
+            break
 
-    of "Addition":
-      stdout.write "First number: "
-      num1 = stdin.readLine.parseFloat
-      stdout.write "Second number: "
-      num2 = stdin.readLine.parseFloat
+        of "Addition":
+            stdout.write "First number: "
+            num1 = stdin.readLine.parseFloat
+            stdout.write "Second number: "
+            num2 = stdin.readLine.parseFloat
 
-      result = num1 + num2
-      echo result
+            result = num1 + num2
+            echo result
 
-    of "Subtraction":
-      stdout.write "First number: "
-      num1 = stdin.readLine.parseFloat
-      stdout.write "Second number: "
-      num2 = stdin.readLine.parseFloat
+        of "Subtraction":
+            stdout.write "First number: "
+            num1 = stdin.readLine.parseFloat
+            stdout.write "Second number: "
+            num2 = stdin.readLine.parseFloat
 
-      result = num1 - num2
-      echo result
+            result = num1 - num2
+            echo result
 
-    of "Multiplication":
-      stdout.write "First number: "
-      num1 = stdin.readLine.parseFloat
-      stdout.write "Second number: "
-      num2 = stdin.readLine.parseFloat
+        of "Multiplication":
+            stdout.write "First number: "
+            num1 = stdin.readLine.parseFloat
+            stdout.write "Second number: "
+            num2 = stdin.readLine.parseFloat
 
-      result = num1 * num2
-      echo result
+            result = num1 * num2
+            echo result
 
-    of "Division":
-      stdout.write "First number: "
-      num1 = stdin.readLine.parseFloat
-      stdout.write "Second number: "
-      num2 = stdin.readLine.parseFloat
+        of "Division":
+            stdout.write "First number: "
+            num1 = stdin.readLine.parseFloat
+            stdout.write "Second number: "
+            num2 = stdin.readLine.parseFloat
 
-      result = num1 / num2
-      echo result
+            result = num1 / num2
+            echo result
 
