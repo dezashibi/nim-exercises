7cb642d766201b3c9cfb0a79459f8751457c379e
Navid Dezashibi <knavels.official@gmail.com>
2022-04-03 15:00:37 +0400

    macro test

---
+++ macro_test.nim
@@ -0,0 +1,34 @@
+import macros
+# dumpTree:
+#   var a = "Test"
+
+macro `:=`(name, value: untyped): untyped =
+  result = newVarStmt(name, value)
+  echo result.repr
+
+a := 0
+
+echo a
+
+macro choiceChecker(statement: untyped): untyped =
+  var
+    branches: seq[(NimNode, NimNode)] #Condition, Body
+    elseBody: NimNode                 #Else code
+  for cond in statement:
+    #Based off the dumpTree, we know this is the else body.
+    let ifBody = cond.findChild(it.kind == nnkStmtList)
+    if cond.kind == nnkInfix:
+      cond.del 3 #Removes Stmtlist
+      branches.add((cond, ifBody))
+    elif cond.kind == nnkCall and $cond[0] == "_":
+      #Based off the dumpTree, we know this is the else body.
+      elseBody = ifBody
+
+  result = newIfStmt(branches) #Generates if stmt
+  result.add newNimNode(nnkElse).add(elseBody) #Appends else body
+  echo result.repr
+
+choiceChecker:
+  11 == 13: echo "Test"
+  12 == 14: echo "Huh"
+  _: echo "duh"
